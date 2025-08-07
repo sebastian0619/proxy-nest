147a7efbb952c98f3539fc5897ba2a29d8b9fc6b
Sebastian <Sebastian-law.lc@outlook.com>
2025-08-07 13:06:28 +0800

    update

--- go-server/proxy/proxy.go
+++ go-server/proxy/proxy.go
@@ -10,10 +10,10 @@ import (
 	"sync"
 	"time"
 
-	"main/cache"
-	"main/config"
-	"main/health"
-	"main/logger"
+	"proxy-nest-go/cache"
+	"proxy-nest-go/config"
+	"proxy-nest-go/health"
+	"proxy-nest-go/logger"
 )
 
 // ProxyResponse 代理响应
